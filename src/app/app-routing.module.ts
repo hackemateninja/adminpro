f41c09cbda625b3b4b4fff08b1bfce20efe37c41
hackemate_ninja <nitrowitty@gmail.com>
2018-12-02 17:51:14 -0600

    child routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { PagesComponent } from './pages/pages.component';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
@@ -7,12 +8,18 @@ import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'graficas1', component: Graficas1Component },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+    ]
+  },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: LoginComponent },
-  { path: 'progress', component: ProgressComponent },
-  { path: 'graficas1', component: Graficas1Component },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   { path: '**', component: NopagefoundComponent }
 ];
 
